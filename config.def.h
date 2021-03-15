1f703ec219d4cd438661cfbc0a7ebeb8fde8254d
Leo Andrade <od.leonardo01@gmail.com>
2021-03-15 02:10:29 -0300

    updating halt script

--- config.def.h
+++ config.def.h
@@ -180,7 +180,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_q,		          spawn,  	   SHCMD(TERMINAL " $HOME/.scripts/dwm/halt/opt_slock") },
 	{ MODKEY|Mod1Mask|ControlMask|ShiftMask,		XK_q,		          spawn,  	   SHCMD(TERMINAL " $HOME/.scripts/dwm/halt/opt_halt") },
 	{ MODKEY|Mod1Mask|ControlMask|ShiftMask,		XK_r,		          spawn,  	   SHCMD(TERMINAL " $HOME/.scripts/dwm/halt/opt_reboot") },
-	{ MODKEY|Mod1Mask|ControlMask|ShiftMask,		XK_h,		          spawn,  	   SHCMD(TERMINAL " $HOME/.scripts/dwm/halt/halt.sh") },
+	{ MODKEY|Mod1Mask|ControlMask|ShiftMask,		XK_h,		          spawn,  	   SHCMD("$HOME/.scripts/dwm/halt/halt.sh") },
 	{ MODKEY,			        XK_w,		          spawn,		   SHCMD("$BROWSER") },
 	{ MODKEY|ControlMask, XK_w,		          spawn,		   SHCMD("chromium") },
 	{ MODKEY|ShiftMask,		XK_w,		          spawn,		   SHCMD(TERMINAL " -e sudo nmtui") },
