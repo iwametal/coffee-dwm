977185e2e6c0ec161850fed2319c6e2b74fae650
Leo Andrade <od.leonardo01@gmail.com>
2021-07-31 16:57:46 -0300

    adding shortcut to gromit-mpx

--- config.h
+++ config.h
@@ -280,6 +280,7 @@ static Key keys[] = {
 	{ MODKEY,			        XK_F9,		       spawn,		      SHCMD("dmenumount") },
 	{ MODKEY,			        XK_F10,		       spawn,		      SHCMD("dmenuumount") },
 	{ MODKEY,			        XK_F11,		       spawn,		      SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
+	{ MODKEY|ControlMask, XK_F9,		       spawn,		      SHCMD("gromit-mpx") },
 	/* { MODKEY,			XK_F12,		xrdb,		{.v = NULL } }, */
 	{ MODKEY,			        XK_space,	       zoom,		      {0} },
 	{ MODKEY|ShiftMask,		XK_space,	       togglefloating,{0} },
